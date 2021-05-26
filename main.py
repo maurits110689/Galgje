6933ec230c9ad881fdd2b241c97a59ec737e341e
Maurits Van <>
2021-05-26 12:05:18 +0000

    Poging beginspel

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welkom bij galgje! Je moet het geheime woord raden voordat je pogingen op zijn!")
+print("Typ een letter en we beginnen!")
+print("Het woord heeft", len(woord) "letters...")
+
+woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+
+ woordenlijst = random.choice(woordenlijst)
